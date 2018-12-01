3ec5889a3375f04ecddbb7de7321014829629f9c
youxinjie <youxinjie@hotmail.com>
2018-12-01 14:50:45 -0600

    Database

--- src/main/java/edu/northwestern/caesar/model/Lecture.java
+++ src/main/java/edu/northwestern/caesar/model/Lecture.java
@@ -1,8 +1,16 @@
 package edu.northwestern.caesar.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
 /**
  * Created by LT on 2018/11/12
  */
+@Getter
+@Setter
+@Entity
 public class Lecture {
 
     private int classroomId;
@@ -10,52 +18,4 @@ public class Lecture {
     private String semester;
     private int year;
     private String classTime;
-
-    public Lecture(int classroomId, int uosCode, String semester, int year, String classTime) {
-        this.classroomId = classroomId;
-        this.uosCode = uosCode;
-        this.semester = semester;
-        this.year = year;
-        this.classTime = classTime;
-    }
-
-    public int getClassroomId() {
-        return classroomId;
-    }
-
-    public void setClassroomId(int classroomId) {
-        this.classroomId = classroomId;
-    }
-
-    public int getUosCode() {
-        return uosCode;
-    }
-
-    public void setUosCode(int uosCode) {
-        this.uosCode = uosCode;
-    }
-
-    public String getSemester() {
-        return semester;
-    }
-
-    public void setSemester(String semester) {
-        this.semester = semester;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getClassTime() {
-        return classTime;
-    }
-
-    public void setClassTime(String classTime) {
-        this.classTime = classTime;
-    }
 }
--- src/main/java/edu/northwestern/caesar/model/Student.java
+++ src/main/java/edu/northwestern/caesar/model/Student.java
@@ -1,49 +1,19 @@
 package edu.northwestern.caesar.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
 /**
  * Created by LT on 2018/11/12
  */
+@Getter
+@Setter
+@Entity
 public class Student {
     private int id;
     private String name;
     private String password;
     private String address;
-
-    public Student(int id, String password) {
-        this.id = id;
-        this.password = password;
-    }
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
