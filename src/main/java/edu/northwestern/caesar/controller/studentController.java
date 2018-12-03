e3a08095532b678819eb7005932315e4c057f6e8
youxinjie <youxinjie@hotmail.com>
2018-12-03 17:21:37 -0600

    merge

--- src/main/java/edu/northwestern/caesar/controller/studentController.java
+++ src/main/java/edu/northwestern/caesar/controller/studentController.java
@@ -1,45 +1,29 @@
 package edu.northwestern.caesar.controller;
 
-import edu.northwestern.caesar.model.Student;
-import edu.northwestern.caesar.repositories.StudentRepository;
-import org.springframework.stereotype.Component;
+import edu.northwestern.caesar.services.StudentService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author LT
  * @Date on 2018/12/2
  */
 @Controller
-public class studentController {
+public class StudentController {
 
-    private final StudentRepository studentRepository;
+    private final StudentService studentService;
 
-    public studentController(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
 
-    @RequestMapping({"/student"})
-    public String stuInfo(Model model){
+//    @RequestMapping({"/student"})
+//    public String stuInfo(Model model){
+//
+//        model.addAttribute("studentInfo", studentService.findAll());
+//
+//        return "student/index";
+//    }
 
-        model.addAttribute("studentInfo", studentRepository.findAll());
 
-        return "student/index";
-    }
-
-    @GetMapping(path="/add") // Map ONLY GET Requests
-    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String password) {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-
-        Student n = new Student();
-        n.setName(name);
-        n.setPassword(password);
-        studentRepository.save(n);
-        return "Saved";
-    }
 }
+
