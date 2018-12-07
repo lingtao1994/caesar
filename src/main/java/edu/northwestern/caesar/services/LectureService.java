0f6459b334082159e52aee26a90827e1677adf90
lingtao <lingtaoshui@gmail.com>
2018-12-06 23:27:42 -0600

    added time helper and implemented time query. leave untested.

--- src/main/java/edu/northwestern/caesar/services/LectureService.java
+++ src/main/java/edu/northwestern/caesar/services/LectureService.java
@@ -2,11 +2,14 @@ package edu.northwestern.caesar.services;
 
 import edu.northwestern.caesar.model.Lecture;
 
+import java.util.List;
+
 /**
  * @author LT
  * @Date on 2018/12/5
  */
 public interface LectureService {
 
-    Lecture findByTime(Long semester, Long year);
+    List<Lecture> findByTime(Long semester, Long year);
+
 }
--- src/main/java/edu/northwestern/caesar/services/LectureServiceImp.java
+++ src/main/java/edu/northwestern/caesar/services/LectureServiceImp.java
@@ -2,7 +2,13 @@ package edu.northwestern.caesar.services;
 
 import edu.northwestern.caesar.model.Lecture;
 import edu.northwestern.caesar.repositories.LectureRepository;
+import edu.northwestern.caesar.tools.CurrentSemester;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.criteria.*;
+import java.util.List;
+
 
 /**
  * @author LT
@@ -12,13 +18,30 @@ import org.springframework.stereotype.Service;
 public class LectureServiceImp implements LectureService {
 
     private final LectureRepository lectureRepository;
+    private final CurrentSemester currentSemester;
 
-    public LectureServiceImp(LectureRepository lectureRepository) {
+    public LectureServiceImp(LectureRepository lectureRepository, CurrentSemester currentSemester) {
         this.lectureRepository = lectureRepository;
+        this.currentSemester = currentSemester;
     }
 
     @Override
-    public Lecture findByTime(Long semester, Long year){
-        return lectureRepository.findByTime(semester, year);
+    public List<Lecture> findByTime(Long semester, Long year){
+
+        return lectureRepository.findAll(new mySpec());
+    }
+
+    private class mySpec implements Specification<Lecture>{
+
+        @Override
+        public Predicate toPredicate(Root<Lecture> root, CriteriaQuery<?> query, CriteriaBuilder cb){
+
+            Path<String> exp1 = root.get("semester");
+            Path<Integer> exp2 = root.get("year");
+
+            Predicate predicate = cb.and(cb.equal(exp1, currentSemester.currentSchoolYear().getSemester()),
+                    cb.equal(exp2, currentSemester.currentSchoolYear().getYear()));
+            return predicate;
+        }
     }
 }
